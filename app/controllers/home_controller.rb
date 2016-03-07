f4b02bc06b35bf8e02bffce280d00706b70160ea
Brent Walther <brentwalther@gmail.com>
2016-03-07 12:03:00 -0600

    Add last snapshotted date to home.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,6 @@ class HomeController < ApplicationController
     return if spotify_user.nil?
 
     @playlists = spotify_user.playlists.map { |p| p.owner.complete!; p }
+    @playlist_snapshots = @playlists.map { |p| Snapshot.joins(:playlist).where("playlists.spotify_id" => p.id).first }
   end
 end
